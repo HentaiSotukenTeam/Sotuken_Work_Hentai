a7191ba836af743652fd2a1836110bc5d7e28cef
kuma_note <Hentai>
2017-10-19 09:37:05 +0900

    10/19 色々

--- app/src/main/java/jp/ac/chiba_fjb/d/real_time_separator_cam/CameraFragment.java
+++ app/src/main/java/jp/ac/chiba_fjb/d/real_time_separator_cam/CameraFragment.java
@@ -5,11 +5,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -50,15 +52,26 @@ public class CameraFragment extends Fragment {
 
 		//撮影ボタン
 		ImageButton photog = (ImageButton)getView().findViewById(R.id.Photo);
-		photog.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				TextView ms = (TextView) getView().findViewById(R.id.ms);
-				ms.setText("撮影しました");
-				HddSave();
+        photog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView ms = (TextView) getView().findViewById(R.id.ms);
+                ms.setText("撮影しました");
+                HddSave();
 
-			}
-		});
+            }
+        });
+
+        Button menu = (Button)getView().findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ダイアログを表示する
+                DialogFragment newFragment = new MenuFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(),null);
+
+            }
+        });
 
 
 
---
+++ app/src/main/java/jp/ac/chiba_fjb/d/real_time_separator_cam/MenuFragment.java
@@ -0,0 +1,70 @@
+package jp.ac.chiba_fjb.d.real_time_separator_cam;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MenuFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        //ホームへmodoru
+        Button homef = (Button)getView().findViewById(R.id.HomeFllow);
+        homef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().cancel();
+                android.support.v4.app.FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main,new HomuhomeFragment());
+                ft.commit();
+            }
+        });
+
+
+        //フォルダ切り替えへmodoru
+        Button folderchenge = (Button)getView().findViewById(R.id.FolderChenge);
+        folderchenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().cancel();
+                android.support.v4.app.FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.layout_main,new FolderSelectFragment());
+                ft.commit();
+            }
+        });
+
+
+
+        //ダイアログを閉じる
+        Button menu = (Button)getView().findViewById(R.id.takeBack);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().cancel();
+
+            }
+        });
+
+
+    }
+
+
+
+}
