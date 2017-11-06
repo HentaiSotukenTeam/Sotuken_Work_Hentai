5dc83bd907773e0f66ec7f80eb2f1ac957b061b2
WIN_NT\x15g013 <x15g013@chiba-fjb.ac.jp>
2017-11-06 11:59:20 +0900

    11/06の最新

---
+++ app/src/main/java/jp/ac/chiba_fjb/d/real_time_separator_cam/OrgaFragment.java
@@ -0,0 +1,27 @@
+package jp.ac.chiba_fjb.d.real_time_separator_cam;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by x15g013 on 2017/11/06.
+ */
+
+public class OrgaFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.orga, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
